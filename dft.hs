b4b31b00701aa08da985afea98e75cc8eb1efd81
Manjunath Sripadarao <manjunath@avyakta.(none)>
2011-12-25 10:31:50 +0530

    Modified the source files based on suggestions from HLint.

--- dft.hs
+++ dft.hs
@@ -13,7 +13,7 @@ import Control.Monad
   
 headerSize bs = (+) 4 (BS.length . BS.concat . Prelude.take 4 . BS.lines $ bs)  
   
-dropHeader bs = BS.drop (headerSize bs) $ bs
+dropHeader bs = BS.drop (headerSize bs) bs
 
 main = do
     [f] <- getArgs
@@ -21,23 +21,41 @@ main = do
     putStrLn "P2"
     putStrLn "# Created by haskell-dft"
     putStrLn "512 512"
-    putStrLn "255"
+    -- putStrLn "255"
     -- DV.mapM print . map ((round::RealFrac a => a -> Int) . magnitude) . (fft2D 512 512) . DV.map (\x -> (x:+0)) . parse $ (dropHeader s)
-    DV.mapM print . scale . map (magnitude) . (ifft2D 512 512) . DV.map (\x -> (x:+0)) . parse $ (dropHeader s)
+    -- DV.map (round::RealFrac a => a -> Int) .
+    --  . scale .
+    -- let xs =  DV.map (round::RealFrac a => a -> Int) . DV.map (log . (+) 1 . magnitude) . fft2D 512 512 . DV.map (\x -> (x:+0)) . parse $ dropHeader s
+    let xs =  DV.map (round::RealFrac a => a -> Int) . DV.map (log . (+) 1 . magnitude) . fft2D 512 512 . DV.map (:+0) . parse $ dropHeader s
+    -- putStrLn (show (DV.maximum xs))
+    print (DV.maximum xs)
+    DV.mapM print xs
     
--- scale::Vector Double -> Vector Double
--- scale xs = map (\x -> (x / maxVec)*255.0) xs
---   where maxVec = DV.sort =<< return xs
+scale::Vector Double -> Vector Double
+scale xs = DV.cons 255.0 (DV.map (\x -> (x/maxVec)*255.0) (DV.tail xs))
+  where
+    maxVec = DV.maximum . DV.tail $ xs -- Take one less than the max, the first value is the max (DC value?)
+    
         
 
+-- findMax::Vector Double -> Double
+-- findMax xs = findMax' xs 0
+--              where
+--                findMax' xs max | DV.null xs = max
+--                                | x > max = findMax' txs x
+--                                | otherwise = findMax' txs max
+--                                  where
+--                                    x = head xs
+--                                    txs = tail xs
+                                   
 rowDft::Int -> Int -> Vector (Complex Double) -> Vector (Complex Double)
-rowDft rows cols b = DV.concatMap (fft_CT . getRow) starts
+rowDft rows cols b = DV.concatMap (fftCT . getRow) starts
                      where
-                       starts = DV.enumFromStepN 0 cols $ rows
+                       starts = DV.enumFromStepN 0 cols rows
                        getRow x = DV.slice x (fromIntegral cols) b
 
 colDft::Int -> Int -> Vector (Complex Double) -> Vector (Complex Double)
-colDft rows cols b = DV.concatMap (fft_CT . getCol) starts
+colDft rows cols b = DV.concatMap (fftCT . getCol) starts
                      where
                        starts = DV.enumFromStepN 0 1 cols
                        getCol x = DV.map ((DV.!) b) (DV.enumFromStepN x cols rows)
@@ -47,13 +65,13 @@ fft2D rows cols xs = colDft rows cols . rowDft rows cols $ xs
 
 
 rowiDft::Int -> Int -> Vector (Complex Double) -> Vector (Complex Double)
-rowiDft rows cols b = DV.concatMap (ifft_CT . getRow) starts
+rowiDft rows cols b = DV.concatMap (ifftCT . getRow) starts
                      where
-                       starts = DV.enumFromStepN 0 cols $ rows
+                       starts = DV.enumFromStepN 0 cols rows
                        getRow x = DV.slice x (fromIntegral cols) b
 
 coliDft::Int -> Int -> Vector (Complex Double) -> Vector (Complex Double)
-coliDft rows cols b = DV.concatMap (ifft_CT . getCol) starts
+coliDft rows cols b = DV.concatMap (ifftCT . getCol) starts
                      where
                        starts = DV.enumFromStepN 0 1 cols
                        getCol x = DV.map ((DV.!) b) (DV.enumFromStepN x cols rows)
@@ -63,38 +81,38 @@ ifft2D rows cols xs = colDft rows cols . rowDft rows cols $ xs
 
 
 dft::Vector (Complex Double) -> Vector (Complex Double)
-dft xs = DV.map (\k -> sum (imap (arg k) xs)) ((numvec n)::Vector (Int))
+dft xs = DV.map (\k -> sum (imap (arg k) xs)) (numvec n :: Vector Int)
   where
     n = length xs
     nf = fromIntegral n
-    numvec n = enumFromStepN 0 1 n
+    numvec = enumFromStepN 0 1
     arg k i x = x * exp (-2 * pi * kf * ifl * (0:+1)/nf)
               where
                 kf = fromIntegral k
                 ifl = fromIntegral i
       
 idft::Vector (Complex Double) -> Vector (Complex Double)
-idft xs = DV.map (\k ->(/) (sum (imap (arg k) xs)) nf) ((numvec n)::Vector (Int))
+idft xs = DV.map (\k ->(/) (sum (imap (arg k) xs)) nf) (numvec n :: Vector Int)
   where
-    n = length xs
+    n = length
     nf = fromIntegral n
-    numvec n = enumFromStepN 0 1 n
+    numvec = enumFromStepN 0 1
     arg k i x = x * exp (2 * pi * kf * ifl * (0:+1)/nf)
               where
                 kf = fromIntegral k
                 ifl = fromIntegral i
 
-fft_CT xs = if (length xs) == 1 then
+fftCT xs = if length xs == 1 then
               dft xs
             else
               let xtop = zipWith3 fft_top fxse fxso (enumFromStepN 0 1 m)
               
                   xbottom = zipWith3 fft_bottom fxse fxso (enumFromStepN 0 1 m)
-                  xse = DV.map ((!) xs) (enumFromStepN 0 2 m)
-                  xso = DV.map ((!) xs) (enumFromStepN 1 2 m)
-                  fxse = fft_CT xse
-                  fxso = fft_CT xso
-                  n = length $ xs
+                  xse = DV.map (xs !) (enumFromStepN 0 2 m)
+                  xso = DV.map (xs !) (enumFromStepN 1 2 m)
+                  fxse = fftCT xse
+                  fxso = fftCT xso
+                  n = length xs
                   nf = fromIntegral n
                   m = n `div` 2
                   fft_top xe xo k = xe + xo * exp (-2 * (0:+1) * pi * k / nf)
@@ -102,18 +120,18 @@ fft_CT xs = if (length xs) == 1 then
               in              
                   (++) xtop xbottom
 
-ifft_CT xs = if (length xs) == 1 then
+ifftCT xs = if length xs == 1 then
                idft xs
             else
                let xtop = zipWith3 fft_top fxse fxso (enumFromStepN 0 1 m)
                    xtop_scaled = DV.map (/2) xtop
                    xbottom = zipWith3 fft_bottom fxse fxso (enumFromStepN 0 1 m)
                    xbottom_scaled = DV.map (/2) xbottom
-                   xse = DV.map ((!) xs) (enumFromStepN 0 2 m)
-                   xso = DV.map ((!) xs) (enumFromStepN 1 2 m)
-                   fxse = ifft_CT xse
-                   fxso = ifft_CT xso
-                   n = length $ xs
+                   xse = DV.map (xs !) (enumFromStepN 0 2 m)
+                   xso = DV.map (xs !) (enumFromStepN 1 2 m)
+                   fxse = ifftCT xse
+                   fxso = ifftCT xso
+                   n = length xs
                    nf = fromIntegral n
                    m = n `div` 2
                    fft_top xe xo k = xe + xo * exp (2 * (0:+1) * pi * k / nf)
